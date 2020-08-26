1f0d704ea47315111c8551cde0d372b97a357c7f
Tiago Aurelio <tiagoaurelio@gmail.com>
2020-08-26 17:25:36 -0300

    Internacionalização da aplicação e pequenos ajustes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum:50 }
     validates :password, length: { minimum:6 }
     validates :email, presence: true, length: { maximum: 255 },
-                                    format: { with: VALID_EMAIL_REGEX },
-                                    uniqueness: { case_sensitive: true }
+                                      format: { with: VALID_EMAIL_REGEX },
+                                      uniqueness: { case_sensitive: true }
 
 end
